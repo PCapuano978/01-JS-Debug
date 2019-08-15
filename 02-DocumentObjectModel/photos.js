a796e3e7c53f1f3d2acec95312d271bc6330cfb9
Patrick Capuano <PCapuano978@west-mec.org>
2019-08-15 15:57:15 -0700

    I'll Continue Later Tonight.

--- 02-DocumentObjectModel/photos.js
+++ 02-DocumentObjectModel/photos.js
@@ -13,6 +13,18 @@
 /* global variables */
 var photoOrder = [1,2,3,4,5];
 
+function populateFigures() {
+   var filename;
+   var currentFig;
+
+   for (var i = 1; i < 4; i++) {
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i-1];
+      currentFig.src = filename;
+   }
+
+}
+
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
    for (var i = 0; i < 5; i++) {
@@ -40,6 +52,39 @@ function leftArrow() {
 /* open center figure in separate window */
 function zoomFig() {
    
+}
+
+// initillizing the "createEventListeners" function for further use.
+function createEventListeners() {
+   var leftarrow = document.getElementById("leftarrow");
+
+      if (leftarrow.addEventListener) {
+      leftarrow.addEventListener("click", leftArrow, false);
+      } else if (leftarrow.attachEvent) {
+      leftarrow.attachEvent("onclick", leftArrow);
+      }
+
+
+   var rightarrow = document.getElementById("rightarrow");
+
+      if (rightarrow.addEventListener) {
+      rightarrow.addEventListener("click", rightArrow, false);
+      } else if (rightarrow.attachEvent) {
+      rightarrow.attachEvent("onclick", rightArrow);
+      }
+
+      var mainFig = document.getElementsByTagName("img")[1];
+      if (mainFig.addEventListener) {
+         mainFig.addEventListener("click", zoomFig, false);
+      }
+      else if (mainFig.attachEvent) {
+         mainFig.attachEvent("onclick", zoomFig);
+      }
+
+      function zoomFig() {
+         alert("center image clicked")
+      }
+
 }
 
 /* create event listeners and populate image elements */
