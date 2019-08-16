e94162fd525217507446ee8ba997e7283fba18c1
Unknown <pcapuano978@west-mec.org>
2019-08-15 23:30:52 -0700

    Finished as much as I'm willing to do for now.
    Will study tomorrow for test.

--- 02-DocumentObjectModel/photos.js
+++ 02-DocumentObjectModel/photos.js
@@ -13,6 +13,10 @@
 /* global variables */
 var photoOrder = [1,2,3,4,5];
 
+var figureCount = 3;
+
+var autoAdvance = setInterval(rightArrow, 5000);
+
 function populateFigures() {
    var filename;
    var currentFig;
@@ -23,7 +27,25 @@ function populateFigures() {
       currentFig.src = filename;
    }
 
+   if (figureCount === 3) {
+      for (var i = 1; i < 4; i++) {
+         filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+            currentFig = document.getElementsByTagName("img")[i-1];
+            currentFig.src = filename;
+      }
+   } else {
+      for (var i = 0; i < 5; i++) {
+         filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+         currentFig = document.getElementsByTagName("img")[i];
+         currentFig.src = filename;
+      }
+   }
+
+   figureCount = 5;
+
 }
+
+
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
@@ -39,6 +61,7 @@ function rightArrow() {
 
 /* shift all images one figure to the right, and change values in photoOrder array to match  */
 function leftArrow() {
+   clearInterval(autoAdvance);
    for (var i = 0; i < 5; i++) {
       if ((photoOrder[i] - 1) === 0) {
          photoOrder[i] = 5;
@@ -49,8 +72,94 @@ function leftArrow() {
    }
 }
 
+function previewFive() {
+
+   var lastFigure = document.createElement("figure");
+
+      function previewFive() {
+
+        lastFigure.id = "fig5";
+        lastFigure.style.zIndex = "5";
+        lastFigure.style.position = "absolute";
+        lastFigure.style.right = "45px";
+        lastFigure.style.top = "67px";
+
+        var lastImage = document.createElement("img");
+
+        lastImage.width = "240";
+        lastImage.height = "135";
+
+        var lastImage = document.createElement("img");
+
+         var articleElem = document.getElementsByTagName("article")[0];
+            
+         lastFigure.appendChild(lastImage);
+
+         articleElem.appendChild(lastFigure);
+         
+         var firstFigure = lastFigure.cloneNode(true);
+
+         firstFigure.id = "fig1";
+         firstFigure.style.right = "";
+         firstFigure.style.left = "45px";
+
+         articleElem.appendChild(firstFigure);
+
+         document.getElementsByTagName("img")[3].src = "images/IMG_0" + photoOrder[4] + "sm.jpg";
+         document.getElementsByTagName("img")[4].src = "images/IMG_0" + photoOrder[0] + "sm.jpg";
+
+         var numberButton = document.querySelector("#fiveButton p"); 
+         numberButton.innerHTML = "show fewer images";
+
+         if (numberButton.addEventListener) {
+            numberButton.removeEventListener("click", previewFive, false);
+            numberButton.addEventListener("click", previewThree, false);
+         }
+         else if (numberButton.attachEvent) {
+            numberButton.detachEvent("onclick", previewFive);
+            numberButton.attachEvent("onclick", previewThree);
+         }
+
+   }
+
+}
+
+function previewThree() {
+   alert("previewThree() called");
+
+   var articleElem = document.getElementsByTagName("article")[0];
+   var numberButton = document.querySelector("#fiveButton p");
+
+   articleElem.removeChild(document.getElementById("fig1"));
+   articleElem.removeChild(document.getElementById("fig5"));
+
+   figureCount = 3;
+   numberButton.innerHTML = "show more images";
+   if (numberButton.addEventListener) {
+      numberButton.removeEventListener("click", previewThree, false);
+      numberButton.addEventListener("click", previewFive, false);
+   }
+   else if (numberButton.attachEvent) {
+      numberButton.detachEvent("onclick", previewThree);
+      numberButton.attachEvent("onclick", previewFive);
+   }
+}
+
 /* open center figure in separate window */
 function zoomFig() {
+
+   var propertyWidth = 960;
+   var propertyHeight = 600;
+   var winLeft = ((screen.width - propertyWidth)/ 2);
+   var winTop = ((screen.height - preopertyHeight)/ 2);
+   var winOptions = "width=960,height=600,";
+   winOptions += ",left=" + winLeft;
+   winOptions += ",top=" + winTop;
+
+   var zoomWindow = window.open("zoom.html", "zoomwin", winOptions);
+
+   var zoomWindow = window.open("zoom.html", "zoomwin", "width=960,height=600");
+   zoomWindow.focus();
    
 }
 
@@ -84,6 +193,14 @@ function createEventListeners() {
       function zoomFig() {
          alert("center image clicked")
       }
+
+   var showAllButton = document.querySelector("#fiveButton p");
+   if (showAllButton.addEventListener) {
+      showAllButton.addEventListener("click", previewFive, false);
+   }
+   else if (showAllButton.attachEvent) {
+      showAllButton.attachEvent("onclick", previewFive);
+   }
 
 }
 
--- 02-DocumentObjectModel/zoom.js
+++ 02-DocumentObjectModel/zoom.js
@@ -2,8 +2,8 @@
 
  *    Photo zoom
  *    Variables and functions
- *    Author: 
- *    Date:   
+ *    Author: Patrick M. Capuano
+ *    Date:   8/15/19
 
  *    Filename: zoom.js
  */
@@ -17,6 +17,21 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 /* populate img element and create event listener */
 function pageSetup() {
    document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+}
+
+function closeWin() {
+   window.close();
+}
+
+function createEventListener() {
+   var closeWindowDiv = document.getElementsByTagName("p")[0];
+   if (closeWindowDiv.addEventListener) {
+      closeWindowDiv.addEventListener("click", closeWin, false);
+   } else if (closeWindowDiv.attachEvent) {
+      closeWindowDiv.attachEvent("onclick", closeWin);
+   }
+
+   createEventListener();
 }
 
 /* add img src value and create event listener when page finishes loading */
